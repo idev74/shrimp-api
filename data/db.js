25557f168f61c42a16f925b641a98536d02fb181
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-05-06 23:41:34 -0700

    linted

--- data/db.js
+++ data/db.js
@@ -6,14 +6,14 @@ assert = require('assert');
 const url = process.env.DB_URL;
 
 mongoose.connect(
-    url,
-    {
-        useNewUrlParser: true, useUnifiedTopology: true,
-    },
-    (err) => {
-        assert.equal(null, err);
-        console.log("Connected successfully to database");
-    }
+  url,
+  {
+    useNewUrlParser: true, useUnifiedTopology: true,
+  },
+  (err) => {
+    assert.equal(null, err);
+    console.log('Connected successfully to database');
+  }
 );
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'));
 mongoose.set('debug', true);
