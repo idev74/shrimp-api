e9dc9886e74d7f8d0b60438b32eaa09bd62ff4ea
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-05-09 22:56:50 -0700

    tests passing

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ require('./controllers/shrimps')(app);
 const port = process.env.PORT;
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server running on port ${port}`);
 });
 
--- test/shrimp.js
+++ test/shrimp.js
@@ -1,96 +1,181 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const {
-  describe, it, before, after
+    describe, it, before, after
 } = require('mocha');
+const mongoose = require('mongoose');
 const app = require('../server');
 const Shrimp = require('../models/shrimp');
 const User = require('../models/user');
 
 const agent = chai.request.agent(app);
+const expect = chai.expect;
+const should = chai.should();
 chai.use(chaiHttp);
+const SAMPLE_OBJECT_ID = mongoose.Types.ObjectId();
 
-describe('Shrimp', () => {
-  const newShrimp = {
-    species: 'neocaridina davidi',
-    color: 'red',
-    age: 1,
-    sex: 'F',
-    owner: '5e8e9b9b0b9b7c1b1c1b1c1b'
-  };
+describe('Shrimp', function () {
+    before(async function () {
+        const user = await chai
+            .request(app)
+            .post('/sign-up')
+            .send({
+                username: 'shrimpluvr',
+                password: 'shrimpinainteasy'
+            });
 
-  const user = {
-    username: 'shrimpluvr',
-    password: 'shrimpinainteasy'
-  };
+        const shrimp = new Shrimp({
+            species: 'species nonexistus',
+            color: 'red',
+            age: 1,
+            sex: 'F',
+            owner: user._id,
+            _id: 'AAAAAAAAAAAA'
+        });
+        try {
+            const savedShrimp = await shrimp.save();
+            this.shrimpId = savedShrimp._id;
+        } catch (err) {
+            console.log(err);
+        }
+    });
 
-  after((done) => {
-    Shrimp.findOneAndDelete(newShrimp)
-      .then(() => {
-        agent.close();
+    after(async function () {
+        try {
+            await User.deleteMany({
+                username: 'shrimpluvr'
+            });
 
-        User
-          .findOneAndDelete({
-            username: user.username
-          })
-          .then(() => {
-            done();
-          })
-          .catch((err) => {
-            done(err);
+            await Shrimp.deleteOne({
+                species: 'species nonexistus'
+            });
+            agent.close();
+        } catch (err) {
+            console.log(err);
+        }
+    });
+
+    it('should not log in a user if not registered', function (done) {
+        chai
+            .request(app)
+            .post('/login')
+            .send({
+                username: 'shrimpluvr',
+                password: 'shrimpinainteasy'
+            })
+            .then(function (res) {
+                res.should.have.status(401);
+                done();
+            })
+            .catch(function (err) {
+                done(err);
+            });
+    });
+
+    it('should sign up a user', function (done) {
+        agent
+            .post('/sign-up')
+            .send({
+                username: 'shrimpluvr',
+                password: 'shrimpinainteasy'
+            })
+            .then(function (res) {
+                res.should.have.status(200);
+                agent.should.have.cookie('nToken');
+                done();
+            })
+            .catch(function (err) {
+                done(err);
+            });
+    });
+
+    describe('GET /shrimps', () => {
+        it('should return all shrimps', (done) => {
+            agent
+                .get('/shrimps')
+                .end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+                    res.should.have.status(200);
+                    res.body.should.be.an('array');
+                    return done();
+                });
+        });
+    });
+
+    describe('POST /shrimps/new', () => {
+        it('should create a new shrimp', (done) => {
+            chai
+                .request(app)
+                .post('/shrimps/new')
+                .send({
+                    species: 'species nonexistus',
+                    color: 'red',
+                    age: 1,
+                    sex: 'F',
+                    owner: this.userId,
+                    _id: 'AAAAAAAAAAAA'
+                })
+                .end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+                    res.body.should.be.an('object');
+                    return done();
+                });
+        });
+    });
+
+    describe('GET /shrimps/:id', () => {
+        it('should return a single shrimp', (done) => {
+            chai
+                .request(app)
+                .get('/shrimps/64572971f913a7683f5f96f9')
+                .end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+                    res.body.should.be.an('object');
+                    return done();
+                });
+        });
+    });
+
+    describe('PUT /shrimps/:id', () => {
+        it('should update a shrimp if user is the owner', async () => {
+          const testShrimp = new Shrimp({
+            species: 'sunkist',
+            color: 'orange',
+            age: 1,
+            sex: 'F',
+            owner: '6456ae71d32efcd0bf8fe831',
           });
+      
+          await testShrimp.save();
+      
+          const res = await chai.request(app)
+            .put(`/shrimps/${testShrimp._id}`)
+            .send({
+              species: 'tangerine tiger',
+            });
+            expect(testShrimp).to.have.property('species', 'sunkist');
+        });
       });
 
-    before((done) => {
-      agent
-        .post('/sign-up')
-        .set('content-type', 'application/x-www-form-urlencoded')
-        .send(user)
-        .then(() => {
-          done();
-        })
-        .catch((err) => {
-          done(err);
+    describe('DELETE /shrimps/:id', () => {
+        it('should delete a shrimp', (done) => {
+            chai
+                .request(app)
+                .delete('/shrimps/64572971f913a7683f5f96f9')
+                .end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+                    res.body.should.be.an('object');
+                    return done();
+                });
         });
     });
-
-    it('should be able to create a shrimp', (done) => {
-      agent
-        .post('/shrimp')
-        .send(newShrimp)
-        .end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
-    });
-
-    it('should be able to get a shrimp', (done) => {
-      agent
-        .get('/shrimp')
-        .end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
-    });
-
-    it('should be able to update a shrimp', (done) => {
-      agent
-        .put('/shrimp')
-        .send(newShrimp)
-        .end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
-    });
-
-    it('should be able to delete a shrimp', (done) => {
-      agent
-        .delete('/shrimp')
-        .send(newShrimp)
-        .end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
-    });
-  });
+      
 });
